df070ebe9929a60ec92676e5d36ffe69edf8e7c7
eduardo1199 <eduardo.soares.116@ufrn.edu.br>
2022-04-12 13:19:42 -0300

    Implementação da autenticação do usuário

---
+++ src/pages/establishments/index.tsx
@@ -0,0 +1,5 @@
+export default function Establishments() {
+  return(
+    <h1>establishments</h1>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,54 @@
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+import { getSession, signIn } from 'next-auth/react';
+
+import { Button } from '@mui/material';
+import { Fastfood, GitHub } from '@mui/icons-material';
+
+import styles from '../styles/Home.module.scss';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello word</h1>
-    </div>
+    <>
+      <Head>SignIn</Head>
+      <main className={styles.main}>
+        <section className={styles.section}>
+          <span>
+            Pronto para o lanche!
+            <h1>
+              Só aqui no SlackTime tem!
+            </h1>
+          </span>
+          <h4>
+            Faça o cadastro na nossa página para curtir seu lanche! 
+          </h4>
+        </section>
+        <div className={styles.containerSignIn}>
+          <Fastfood />
+          <Button onClick={() => signIn('github', )}>
+            Entrar <GitHub />
+          </Button>
+        </div>
+      </main>
+    </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if(session) {
+    return {
+      redirect: {
+        destination: '/establishments',
+        permanent: false
+      }
+    }
+  }
+  
+  return {
+    props: {
+
+    }
+  }
 }
