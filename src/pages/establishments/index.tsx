1506ca5ed3141c695252b8743f968e2ebfb7544b
eduardo1199 <eduardo.soares.116@ufrn.edu.br>
2022-04-14 22:09:55 -0300

    Adicionar requisição para estabelecimentos

--- src/pages/establishments/index.tsx
+++ src/pages/establishments/index.tsx
@@ -1,10 +1,36 @@
+import { GetStaticProps } from 'next';
 import { Header } from '../../components/Header/Header';
+import { api } from '../../services/api';
 
-export default function Establishments() {
+interface Establishments {
+  establishments: {
+    id: number;
+		name: string;
+		address: string;
+		type: string;
+		description: string;
+		slug: string;
+  }[]
+}
+
+export default function Establishments({ establishments }: Establishments) {
+
+  console.log(establishments);
+
   return(
     <>
       <Header />
-      <h1>establishments</h1>
+      <div>establishments</div>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await api.get('estabelecimentos');
+
+  return {
+    props: {
+      establishments: response.data
+    }
+  }
 }
